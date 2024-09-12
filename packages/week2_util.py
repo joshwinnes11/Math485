a1932f13b2b0659ae427e26ddb4feb495316e43d
Josh Winnes <joshwinnes@icloud.com>
2024-09-12 12:54:43 -0500

    quick commit before end of class

---
+++ packages/week2_util.py
@@ -0,0 +1,12 @@
+""" 
+Utility functions for working with Allegheny DHS Synthetic Data
+    - @author Josh Winnes
+    - PRECONDITIONS: various parameters
+    - POSTCONDITIONS: various
+    - PARAMETERS: various
+
+"""
+
+import pandas as pd
+import numpy as np
+
