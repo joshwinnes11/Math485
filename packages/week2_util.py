077db812fbc6494eb2058fb6680fc223dbd7e68f
Josh Winnes <joshwinnes@icloud.com>
2024-09-18 17:30:01 -0500

    finishing up week2

--- packages/week2_util.py
+++ packages/week2_util.py
@@ -7,37 +7,21 @@ Utility functions for working with Allegheny DHS Synthetic Data
 
 """
 
-import pandas as pd
-import numpy as np
-
-def get_retention_ratio_matrix(df, recipient):
+def get_sum_associated_servs(cohort_services):
     
-    df_retention = pd.merge(df, recipient[['id','first_date']], on = 'id', how = 'left')
-    df_retention['elapsed'] = df_retention['date'].dt.month - df_retention['first_date'].dt.month
-    df_retention_count = df_retention.groupby(["first_date", "elapsed"]).agg(
-    active = ("id", "nunique"),
-    ).reset_index()
-    df_retention_count = df_retention_count.pivot(index = "first_date", columns="elapsed", values='active')
-    df_retention_ratio = df_retention_count.reset_index()
-    df_retention_ratio = df_retention_count.div(df_retention_ratio.iloc[:,1].to_numpy(),axis = 0)
-    
-    return df_retention_ratio
-
-
-def get_correlation_matrix(df):
-    
-    df_temp = df.groupby(["id","serv"]).agg(
+    df_temp_cohort = cohort_services.groupby(["id","serv"]).agg(
     num_serv = ('service', 'nunique') # this will be 1 or 0, "service" is categorical 
     ).reset_index()
-    
-    df_serv = df_temp.pivot_table(
+
+    df_serv_cohort = df_temp_cohort.pivot_table(
     values='num_serv', 
     index=["id"], columns="serv", aggfunc=np.sum
     ).reset_index()
+
+    list_cohort_servs = pd.DataFrame(df_serv_cohort.iloc[:,1:22].sum())
+    list_cohort_servs.columns = ['count']
+    return list_cohort_servs
     
-    correlation = df_serv.iloc[:,1:23].corr(method="spearman")
-    
-    return correlation
 
 
     
